1d39443327986efdd39c243f7732b31db7cd6f62
Ashley Flynn <ashnflynn@gmail.com>
2026-02-20 18:59:20 -0600

    Step 3 complete, output in console: 83

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const tags = ["js", "week3"];
-
 const scores = [10, 25, 18, 30];
+let totalScore = 0; // Initialize "accumulator"
+
+for (let i = 0; i < scores.length; i++){ 
+    totalScore += scores[i]; //Add each item from array "scores" to the sum
+};
+console.log(totalScore); //Output: 83
 
