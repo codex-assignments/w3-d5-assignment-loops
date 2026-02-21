42adcdc699195e44990b70b6f4ac8dc677ae14b9
Ashley Flynn <ashnflynn@gmail.com>
2026-02-20 19:27:52 -0600

    added notes

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const tags = ["js", "week3"];
 const scores = [10, 25, 18, 30];
 let totalScore = 0; // Initialize "accumulator"
 
-for (let i = 0; i < scores.length; i++){ 
-    totalScore += scores[i]; //Add each item from array "scores" to the sum
-};
+for (let i = 0; i < scores.length; i++) {
+  totalScore += scores[i]; //Add each item from array "scores" to the sum
+}
 console.log(totalScore); //Output: 83
 
 tags.forEach(function (tag) {
   console.log(tag);
 });
 
+//Map will take each value of an array and transform it by the logic in the function placed in () of "array".map()
+
 const doubledScores = scores.map((score) => {
-    return score *2
-})
+  return score * 2;
+});
 
 console.log(doubledScores);
-
